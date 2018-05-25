f05b3f2ed76eb2589c2313cbc842966ebe18dd57
hmq201211 <879602728@qq.com>
2018-05-25 12:09:34 +0800

    5.25

--- summerTest/src/test/Test.java
+++ summerTest/src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         String a = null;
-        System.out.println(a);
+        System.out.println(a + 2);
     }
 }
